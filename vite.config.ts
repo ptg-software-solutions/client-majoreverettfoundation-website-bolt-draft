a62ea8591279cb31493b2be3f43b7c3841dddb05
craiglrumph <120511975+craiglrumph@users.noreply.github.com>
2026-06-22 04:49:17 -0400

    Update vite.config.ts base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/client-majoreverettfoundation-website/',
+  base: '/client-majoreverettfoundation-website-bolt-draft/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
